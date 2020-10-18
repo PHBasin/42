c75188dceeb3812ad7a2918122b908df5f0400fb
Pierre-henri BASIN <pbasin@student.42.fr>
2020-10-18 09:19:02 +0200

    added

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: pbasin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 18:15:29 by pbasin            #+#    #+#             */
-/*   Updated: 2020/10/17 19:55:37 by pbasin           ###   ########.fr       */
+/*   Updated: 2020/10/18 09:18:39 by pbasin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,16 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void ft_more_or_less(char c)
+void	ft_print_hex(char c)
 {
-    if(c < 10)
-    {
-        c += 48;
-    }
-    else
-    {
-        c += 55;
-    }
+	char base[17];
+
+	base = "0123456789abcdef";
+	ft_putchar(base[c / 16]);
+	ft_putchar(base[c % 16]);
 }
 
-void ft_print_hex(char c)
-{
-    while (c / 16 != 0)
-    {
-        c /= 16;
-            if (c < 16)
-                ft_putchar(c);
-            else
-                ft_more_or_less(c);
-        c %= 16;
-    }
-}
-
-void ft_putstr_non_printable(char *str)
+void	ft_putstr_non_printable(char *str)
 {
 	int i;
 
@@ -51,7 +35,7 @@ void ft_putstr_non_printable(char *str)
 	{
 		if (str[i] < 32 || str[i] > 126)
 		{
-		    ft_putchar('\\');
+			ft_putchar('\\');
 			ft_print_hex(str[i]);
 		}
 		else
@@ -61,13 +45,3 @@ void ft_putstr_non_printable(char *str)
 		i++;
 	}
 }
-
-int main(void)
-{
-    char str[80] = "salut, comment tu vas \n? 42mots quarante-deux; cinquante+et+un";
-    ft_putstr_non_printable(str);
-    
-    return (0);
-}
-
-
