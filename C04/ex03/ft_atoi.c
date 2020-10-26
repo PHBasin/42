d0aa8dbf336480a7c8a906ddbf41e976f6d8b48e
Pierre-henri BASIN <pbasin@student.42.fr>
2020-10-26 14:24:29 +0100

    added

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,46 +6,33 @@
 /*   By: pbasin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 18:09:58 by pbasin            #+#    #+#             */
-/*   Updated: 2020/10/21 14:58:20 by pbasin           ###   ########.fr       */
+/*   Updated: 2020/10/26 14:23:47 by pbasin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_find_num(char *str)
+int ft_atoi(char *str)
 {
-	while ((*str > 8 && *str < 14) || *str == 32 || *str == 45 || *str == 43)
-		str++;
-	return (str);
-}
-
-int		ft_dash_nb(char *str)
-{
+	int neg;
+	int n;
 	int i;
 
-	i = 1;
-	while (*str)
+	while ((*str > 8 && *str < 14) || *str == 32)
+		str++;
+	neg = 1;
+	while (*str == 45 || *str == 43)
 	{
 		if (*str == 45)
-			i *= -1;
+			neg *= -1;
 		str++;
 	}
-	return (i);
-}
-
-int		ft_atoi(char *str)
-{
-	char	*nums;
-	int		i;
-	int		n;
-
-	nums = ft_find_num(str);
 	i = 0;
 	n = 0;
-	while (nums[i] > 48 && nums[i] < 57 && nums)
+	while (str[i] > 47 && str[i] < 58)
 	{
-		n += nums[i] - 48;
-		if (nums[i + 1] > 48 && nums[i + 1] < 57)
+		n += str[i] - 48;
+		if (str[i + 1] > 48 && str[i + 1] < 57)
 			n *= 10;
 		i++;
 	}
-	return (n * ft_dash_nb(str));
+	return (n * neg);
 }
