029588f0744d285efe88bd0953e71de56ee21b8d
Pierre-henri BASIN <pbasin@student.42.fr>
2020-10-28 11:20:25 +0100

    added

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: pbasin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 07:37:45 by pbasin            #+#    #+#             */
-/*   Updated: 2020/10/28 08:53:21 by pbasin           ###   ########.fr       */
+/*   Updated: 2020/10/28 11:19:40 by pbasin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,30 @@ int		ft_in_base(char c, char *base);
 int		ft_check_base(char *base);
 int		ft_atoi_base(char *str, char *base);
 
+int		ft_bases_checked(char *base_to, char *base_from)
+{
+	int is_base_to_ok;
+	int is_base_from_ok;
+
+	is_base_to_ok = ft_check_base(base_to);
+	is_base_from_ok = ft_check_base(base_from);
+	if (!(is_base_to_ok && is_base_from_ok))
+		return (0);
+	return (is_base_to_ok);
+}
+
 int		ft_num_size(int n, int size_base_to)
 {
 	int i;
 
 	i = 0;
+	if (n < 0)
+	{
+		n = -n;
+		i = 2;
+	}
+	else
+		i = 1;
 	while (n > size_base_to)
 	{
 		n /= size_base_to;
@@ -32,26 +51,26 @@ int		ft_num_size(int n, int size_base_to)
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	char	*num;
-	int		size_base_to;
 	int		n;
 	int		i;
 	int		neg;
 
 	neg = 0;
-	size_base_to = ft_check_base(base_to);
+	if (!ft_bases_checked(base_to, base_from))
+		return (NULL);
 	n = ft_atoi_base(nbr, base_from);
+	i = ft_num_size(n, ft_bases_checked(base_to, base_from));
 	if (n < 0)
 	{
 		n = -n;
 		neg = 1;
 	}
-	i = ft_num_size(n, size_base_to);
 	num = malloc(sizeof(*num) * (i + 1));
-	num[i++] = '\0';
+	num[i + neg] = '\0';
 	while (--i > -1)
 	{
-		num[i] = base_to[n % size_base_to];
-		n /= size_base_to;
+		num[i] = base_to[n % ft_bases_checked(base_to, base_from)];
+		n /= ft_bases_checked(base_to, base_from);
 	}
 	if (neg)
 		num[0] = '-';
