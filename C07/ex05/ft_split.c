c46f70cfdc9fc183e3da2147f818f53e75396b6e
Pierre-henri BASIN <pbasin@student.42.fr>
2020-10-28 10:50:01 +0100

    added

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -6,10 +6,18 @@
 /*   By: pbasin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 07:38:55 by pbasin            #+#    #+#             */
-/*   Updated: 2020/10/26 10:15:05 by pbasin           ###   ########.fr       */
+/*   Updated: 2020/10/28 10:45:24 by pbasin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+char	strcpy()
+{
+
+}
+
 char	**ft_split(char *str, char *charset)
 {
+
 }
