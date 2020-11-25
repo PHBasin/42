e7629ed2aae9e314fd11f6aa6297deeda96aacb1
PHBasin <pierre-henri.basin@orange.fr>
2020-11-25 14:31:45 +0100

    added

--- C10/ex01/ft_cat.c
+++ C10/ex01/ft_cat.c
@@ -6,15 +6,15 @@
 /*   By: phbasin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:15:24 by phbasin           #+#    #+#             */
-/*   Updated: 2020/11/21 20:58:22 by phbasin          ###   ########.fr       */
+/*   Updated: 2020/11/25 14:26:52 by phbasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libgen.h>	// basename
-#include <errno.h>	// errno
-#include <unistd.h>	// close read write
-#include <string.h>	// strerror
-#include <fcntl.h>	// open
+#include <libgen.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 void	ft_putstr(int fd, char *str)
 {
@@ -22,32 +22,46 @@ void	ft_putstr(int fd, char *str)
 		write(fd, str++, 1);
 }
 
-int		ft_displayfile(char *path)
+void	ft_puterr(char *progname, char *path, char *error)
 {
+	ft_putstr(2, progname);
+	ft_putstr(2, ": ");
+	ft_putstr(2, path);
+	ft_putstr(2, ": ");
+	ft_putstr(2, error);
+	ft_putstr(2, "\n");
+}
+
+int	ft_displayfile(char *path)
+{
+	char	buf[2048];
 	int		fd;
 	int		len;
-	char	buf[29696];
 
-	fd = open(path, O_RDONLY);
+	fd = path ? open(path, O_RDONLY) : 0;
 	if (fd == -1)
 		return (-1);
-	len = read(fd, buf, 29696);
-	ft_putstr(1, buf);
-	close(fd);
-	return (len);
+	while ((len = read(fd, buf, 2048)) > 0)
+		write(1, buf, len);
+	if (fd != 0)
+		close(fd);
+	return (len ? -1 : 0);
 }
 
 int		main(int argc, char *argv[])
 {
+	char 	*progname;
 	int		i;
 
+	progname = basename(argv[0]);
 	if (argc < 2)
-		ft_putstr(2, strerror(errno));
+		ft_displayfile(NULL);
 	i = 1;
-	while (i++ < argc)
+	while (i < argc)
 	{
 		if (ft_displayfile(argv[i]) < 0)
-			ft_putstr(2, strerror(errno));
+			ft_puterr(progname, argv[i], strerror(errno));
+		i++;
 	}
 	return (0);
 }
