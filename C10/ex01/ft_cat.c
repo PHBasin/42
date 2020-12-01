77f2bd5257d5434ff0e398051749d2af1fbc77b3
PHBasin <pierre-henri.basin@orange.fr>
2020-12-01 15:54:28 +0100

    added

--- C10/ex01/ft_cat.c
+++ C10/ex01/ft_cat.c
@@ -6,7 +6,7 @@
 /*   By: phbasin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:15:24 by phbasin           #+#    #+#             */
-/*   Updated: 2020/11/25 14:50:22 by phbasin          ###   ########.fr       */
+/*   Updated: 2020/12/01 11:48:17 by phbasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,17 @@ void	ft_putstr(int fd, char *str)
 	while (*str)
 		write(fd, str++, 1);
 }
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
 
 void	ft_puterr(char *progname, char *path, char *error)
 {
@@ -56,12 +67,13 @@ int		main(int argc, char *argv[])
 	progname = basename(argv[0]);
 	if (argc < 2)
 		ft_displayfile(NULL);
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (++i < argc)
 	{
+		if (!ft_strcmp(argv[i], "-"))
+			argv[i] = NULL;
 		if (ft_displayfile(argv[i]) < 0)
 			ft_puterr(progname, argv[i], strerror(errno));
-		i++;
 	}
 	return (0);
 }
--- C10/ex02/ft_tail.c
+++ C10/ex02/ft_tail.c
@@ -6,13 +6,22 @@
 /*   By: phbasin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 09:17:30 by phbasin           #+#    #+#             */
-/*   Updated: 2020/11/29 21:07:31 by phbasin          ###   ########.fr       */
+/*   Updated: 2020/12/01 11:14:45 by phbasin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+void	ft_clean_buf(char *buf, int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+		buf[i++] = 0;
+}
 
 void	ft_default_display(char *buf, int len)
 {
@@ -43,7 +52,10 @@ int		ft_open_file(char* path)
 	int		fd;
 	int		len;
 
+	ft_clean_buf(buf, 2048);
 	fd = path ? open(path, O_RDONLY) : 0;
+	if (fd < 0)
+		return (-1);
 	len = read(fd, buf, 2048);
 	ft_default_display(buf, len);
 	if (fd != 0)
@@ -56,6 +68,8 @@ int		main(int argc, char *argv[])
 	int i;
 
 	i = 1;
+	if (i >= argc)
+		ft_open_file(NULL);
 	while (i < argc)
 		ft_open_file(argv[i++]);
 	return (0);
